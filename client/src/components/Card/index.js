f867bcf5069bc88de5c97aa8d64376c8c9621718
Benjamin Brier <ben.brier14@gmail.com>
2020-01-08 15:45:42 -0500

    completed some of the components

---
+++ client/src/components/Card/index.js
@@ -0,0 +1,21 @@
+//-----Import-----//
+import React from "react";
+
+//-----Card Component-----//
+function Card({ icon, title, children }) {
+  return (
+    <div className="card mt-4">
+      <div className="card-header">
+        <h3>
+          <strong>
+            <i className={`fa fa-${icon}`} aria-hidden="true" /> {title}
+          </strong>
+        </h3>
+      </div>
+      <div className="card-body">{children}</div>
+    </div>
+  );
+}
+
+//-----Export-----//
+export default Card;
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+//-----Import-----//
+import React from "react";
+
+//-----Footer Component-----//
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fab fa-github" /> Proudly built using React.js
+      </p>
+    </footer>
+  );
+}
+
+//-----Export-----//
+export default Footer;
