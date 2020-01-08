42ee940bd8b2413a7d3a4ca8b47c6ee6179b7b53
Benjamin Brier <ben.brier14@gmail.com>
2020-01-08 14:57:39 -0500

    book api routes complete

---
+++ routes/api/books.js
@@ -0,0 +1,18 @@
+//-----Require-----//  
+const router = require("express").Router();
+const bookController = require("../../controllers/bookController");
+
+//-----Base Route to Find/Create-----//
+router.route("/")
+  .get(bookController.findAll)
+  .post(bookController.create);
+
+//-----Route to See Current Json-----//
+router
+  .route("/:id")
+  .get(bookController.findById)
+  .put(bookController.update)
+  .delete(bookController.remove);
+
+//-----Export-----//
+module.exports = router;
