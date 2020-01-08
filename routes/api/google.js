40dfae612e05124737d58cb07a10eda09738b722
Benjamin Brier <ben.brier14@gmail.com>
2020-01-08 14:52:52 -0500

    google api route complete

---
+++ routes/api/google.js
@@ -0,0 +1,11 @@
+//-----Require-----//
+const router = require("express").Router();
+const googleController = require("../../controllers/googleController");
+
+//-----Home Route uses Google Controller-----//
+router
+  .route("/")
+  .get(googleController.findAll);
+
+//-----Export-----//
+module.exports = router;
